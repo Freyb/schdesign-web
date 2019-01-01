35d285e6b44b2c3be7ebac02ed9a6dc5c71fd6ee
Balazs <balazs.frey@gmail.com>
2019-01-01 22:18:36 +0100

    Create OrderModal

---
+++ src/components/OrderModal.tsx
@@ -0,0 +1,90 @@
+import { graphql, StaticQuery } from 'gatsby';
+import React from 'react';
+import { Heading, Text } from 'rebass';
+import Container from './Container';
+import { Props } from './Section';
+import ListItem from './ListItem';
+import List from './List';
+
+const OrderModal = (props: Props) => (
+  <Container as={Text} textAlign="left">
+    <Heading id="order" textAlign="center" mb={4}>
+      Szabályzat
+    </Heading>
+
+    <List
+      {...props}
+      css="
+          display: flex;
+          flex-wrap: wrap;
+          padding: 0;
+          list-style: none;
+        "
+    >
+      <StaticQuery
+        query={graphql`
+          {
+            allRulesYaml {
+              edges {
+                node {
+                  id
+                  name
+                }
+              }
+            }
+          }
+        `}
+        render={data =>
+          data.allRulesYaml.edges.map(({ node }: any, index: number) => (
+            <ListItem
+              key={node.id}
+              css={`
+                flex-basis: 100%;
+                margin-bottom: 1.5rem;
+                @media (min-width: 1024px) {
+                  flex-basis: 50%;
+                  padding: 0 1rem;
+                }
+                display: flex;
+              `}
+            >
+              <Text
+                as="div"
+                css="
+                      flex: 0 0 4rem;
+                      margin-right: 0.5rem;
+                    "
+              >
+                <Text
+                  as="div"
+                  css="
+                      width: 4rem;
+                      height: 4rem;
+                      font-size: 3rem;
+                      font-family: 'Roboto Mono, monospace';
+                      line-height: 4rem;
+                      text-align: center;
+                      background-color: #f8485e;
+                      border-radius: 250px;
+                    "
+                >{`${index < 9 ? `0` : ``}${index + 1}`}</Text>
+              </Text>
+              <Text
+                css={`
+                  align-self: center;
+                  @media (min-width: 1024px) {
+                    align-self: flex-start;
+                  }
+                `}
+              >
+                {node.name}
+              </Text>
+            </ListItem>
+          ))
+        }
+      />
+    </List>
+  </Container>
+);
+
+export default OrderModal;
--- src/components/OrderSection.tsx
+++ src/components/OrderSection.tsx
@@ -1,92 +1,34 @@
-import { graphql, StaticQuery } from 'gatsby';
-import React from 'react';
+import React, { useState } from 'react';
 import { Heading, Text } from 'rebass';
+import Modal from 'react-responsive-modal';
 import Container from './Container';
 import Section, { Props } from './Section';
-import ListItem from './ListItem';
-import List from './List';
+import OrderModal from './OrderModal';
+import Paragraph from './Paragraph';
 
-const OrderSection = (props: Props) => (
-  <Section {...props}>
-    <Container as={Text} textAlign="left">
-      <Heading id="order" textAlign="center" my={4}>
-        Megrendelés
-      </Heading>
+const OrderSection = (props: Props) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
-      <List
-        {...props}
-        css="
-          display: flex;
-          flex-wrap: wrap;
-          padding: 0;
-          list-style: none;
-        "
-      >
-        <StaticQuery
-          query={graphql`
-            {
-              allRulesYaml {
-                edges {
-                  node {
-                    id
-                    name
-                  }
-                }
-              }
-            }
-          `}
-          render={data =>
-            data.allRulesYaml.edges.map(({ node }: any, index: number) => (
-              <ListItem
-                key={node.id}
-                css={`
-                  flex-basis: 100%;
-                  margin-bottom: 1.5rem;
-                  @media (min-width: 1024px) {
-                    flex-basis: 50%;
-                    padding: 0 1rem;
-                  }
-                  display: flex;
-                `}
-              >
-                <Text
-                  as="div"
-                  css="
-                      flex: 0 0 4rem;
-                      margin-right: 0.5rem;
-                    "
-                >
-                  <Text
-                    as="div"
-                    css="
-                      width: 4rem;
-                      height: 4rem;
-                      font-size: 3rem;
-                      font-family: 'Roboto Mono, monospace';
-                      line-height: 4rem;
-                      text-align: center;
-                      background-color: #f8485e;
-                      border-radius: 250px;
-                    "
-                  >{`${index < 9 ? `0` : ``}${index + 1}`}</Text>
-                </Text>
-                <Text
-                  css={`
-                    align-self: center;
-                    @media (min-width: 1024px) {
-                      align-self: flex-start;
-                    }
-                  `}
-                >
-                  {node.name}
-                </Text>
-              </ListItem>
-            ))
-          }
-        />
-      </List>
-    </Container>
-  </Section>
-);
+  const closeModal = () => setModalIsOpen(false);
+  const openModal = () => setModalIsOpen(true);
+
+  return (
+    <Section {...props}>
+      <Container as={Text} textAlign="left">
+        <Heading id="order" textAlign="center" my={4}>
+          Megrendelés
+        </Heading>
+        <Paragraph>Lorem ipsum</Paragraph>
+        <button type="button" onClick={openModal}>
+          Szabályzat
+        </button>
+        {/* There is a bug with the scrolling. Probably will be fixed soon */}
+        <Modal open={modalIsOpen} onClose={closeModal} center>
+          <OrderModal />
+        </Modal>
+      </Container>
+    </Section>
+  );
+};
 
 export default OrderSection;
