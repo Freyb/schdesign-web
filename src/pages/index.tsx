d67d72886cc3c80ac8d3039e94596642c43e66b6
Balazs <balazs.frey@gmail.com>
2019-01-01 20:11:16 +0100

    Create OrderSection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,14 @@ import AboutSection from '../components/AboutSection';
 import ContactSection from '../components/ContactSection';
 import Layout from '../components/Layout';
 import WorksSection from '../components/WorksSection';
+import OrderSection from '../components/OrderSection';
 
 const IndexPage = () => (
   <Layout>
     <AboutSection />
     <WorksSection variant="dark" />
     <ContactSection my={[4, 5]} />
+    <OrderSection />
   </Layout>
 );
 
