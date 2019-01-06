84b53303df35b8d140d01b65d949fcb015f9810c
Balazs <balazs.frey@gmail.com>
2019-01-06 18:22:45 +0100

    Create details view

--- src/components/NewWorkModal.tsx
+++ src/components/NewWorkModal.tsx
@@ -21,31 +21,57 @@ type Props = {
   newWorkCb: (renderType: number, date: Date, desc: string) => void;
 };
 
-const NewWorkModal = ({ images, newWorkCb }: Props) => (
-  <Container as={Text} textAlign="left">
-    <Heading id="order" textAlign="center" mb={4}>
-      Új Munka
-    </Heading>
-    <List px={0} css="display: flex; flex-wrap: wrap;">
-      {images.map((node: any, index: number) => (
-        <Box
-          as="li"
-          key={node.name}
-          flex={['50%', '33%']}
-          px={1}
-          py={1}
-          css="display: flex; align-items: center; border: solid 0px red;"
-          onClick={() => newWorkCb(index)}
-        >
+const NewWorkModal = ({ images, newWorkCb }: Props) => {
+  const [renderType, setRender] = useState(-1);
+  const [date, setDate] = useState(new Date());
+  const [desc, setDesc] = useState('');
+
+  const handleOk = () => newWorkCb(renderType, date, desc);
+
+  return (
+    <Container as={Text} textAlign="left">
+      <Heading id="order" textAlign="center" mb={4}>
+        Új {`${renderType < 0 ? `Munka` : images[renderType].label}`}
+      </Heading>
+
+      {renderType < 0 && (
+        <List px={0} css="display: flex; flex-wrap: wrap;">
+          {images.map((node: any, index: number) => (
+            <Box
+              as="li"
+              key={node.name}
+              flex={['50%', '33%']}
+              px={1}
+              py={1}
+              css="display: flex; align-items: center; border: solid 0px red;"
+              onClick={() => setRender(index)}
+            >
+              <Box as="div" css="width: 100%; border: solid 1px black;">
+                <Box as={Img} fluid={node} m={1} />
+                <Paragraph>{node.label}</Paragraph>
+              </Box>
+            </Box>
+          ))}
+        </List>
+      )}
+      {renderType >= 0 && (
+        <Box as="div">
           <Box as="div" css="width: 100%; border: solid 1px black;">
-            <Box as={Img} fluid={node} m={1} />
-            <Paragraph>{node.label}</Paragraph>
+            <input type="button" value="back" onClick={() => setRender(-1)} />
+            <Box as={Img} fluid={images[renderType]} m={1} />
+            <Paragraph>{images[renderType].label}</Paragraph>
+            <DatePicker selected={date} onChange={setDate} />
+            <Box
+              as="textarea"
+              value={desc}
+              onChange={(e: any) => setDesc(e.target.value)}
+            />
+            <input type="button" value="ok" onClick={handleOk} />
           </Box>
         </Box>
-      ))}
-    </List>
-    )
-  </Container>
-);
+      )}
+    </Container>
+  );
+};
 
 export default NewWorkModal;
